437cc97bf7de356723bb281c7de237a242ec0c27
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-21 18:09:57 +0530

    feat:userInput.rs file added

---
+++ Introduction/userInput.rs
@@ -0,0 +1,12 @@
+use std::io::{self, Write};
+
+fn main() {
+    let mut s = String::new();
+
+    print!("Please enter your name: ");
+    io::stdout().flush().unwrap(); // Flush stdout to display the print message immediately
+
+    io::stdin().read_line(&mut s).expect("Failed to read line"); // Read user input
+
+    println!("Your name is: {}", s.trim());
+}
