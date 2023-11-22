c9989423767dfc464055b82d183d8acd63461d1f
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-22 09:56:33 +0530

    feat:example3 added

--- Introduction/userInput.rs
+++ Introduction/userInput.rs
@@ -21,3 +21,21 @@ fn main() {
 }
 
 //Example:3
+use std::io;
+
+fn main() {
+    let mut input_a = String::new();
+    let mut input_b = String::new();
+
+    io::stdin().read_line(&mut input_a).unwrap();
+    io::stdin().read_line(&mut input_b).unwrap();
+
+    let a: i32 = input_a.trim().parse().unwrap();
+    let b: i32 = input_b.trim().parse().unwrap();
+
+    // complete the code  
+    let sum=a+b;
+    let diff=a-b;
+    println!("Sum is: {}",sum);
+    println!("Difference is: {}",diff);
+}
