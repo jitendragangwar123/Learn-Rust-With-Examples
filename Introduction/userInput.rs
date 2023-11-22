c3c25dec5a762699c35b82bb936dd7faa729fc96
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-22 09:50:35 +0530

    feat:Example2 added

--- Introduction/userInput.rs
+++ Introduction/userInput.rs
@@ -1,5 +1,5 @@
+//Example:1
 use std::io::{self, Write};
-
 fn main() {
     let mut s = String::new();
 
@@ -10,3 +10,14 @@ fn main() {
 
     println!("Your name is: {}", s.trim());
 }
+
+//Example:2
+use std::io;
+fn main() {
+    let mut a = String::new();
+    io::stdin().read_line(&mut a).unwrap(); // replace  the __
+    let a: i32 = a.trim().parse().unwrap();
+    println!("Your number is: {}", a); // replace the __
+}
+
+//Example:3
