f2fba69cfcd201f6c46ed408eca804cbc3dc5588
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-27 12:04:59 +0530

    feat:helloWorld.rs file added

---
+++ Introduction/helloWorld.rs
@@ -0,0 +1,19 @@
+/*
+Rust:
+  1. Rust is a modern systems programming language focusing on safety, speed, and concurrency. 
+  2. It accomplishes these goals by being memory safe without using garbage collection.
+*/
+
+/*
+Note: `println!` is a macro that prints text to the console.
+*/
+
+//helloWorld.rs
+// This is the main function.
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+    // Print text to the console.
+    println!("Hello World!");
+}
+
+// Output :- Hello World!
