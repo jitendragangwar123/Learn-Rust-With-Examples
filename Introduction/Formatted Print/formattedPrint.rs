fb310c6e2e249b1de71c9d372543df31f7d4ff9c
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-27 15:38:38 +0530

    feat:formattedPrint.rs file added

--- Introduction/Formatted Print/formattedPrint.rs
+++ Introduction/Formatted Print/formattedPrint.rs
@@ -1,3 +1,13 @@
+/*
+ Printing is handled by a series of macros defined in std::fmt
+   format!: write formatted text to String
+   print!: same as format! but the text is printed to the console (io::stdout).
+   println!: same as print! but a newline is appended.
+   eprint!: same as print! but the text is printed to the standard error (io::stderr).
+   eprintln!: same as eprint! but a newline is appended.
+*/
+
+
 fn main() {
     // In general, the `{}` will be automatically replaced with any
     // arguments. These will be stringified.
