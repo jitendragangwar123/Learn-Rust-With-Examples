5e365c75af09b83bdda5ba15d33c3bebbfdc7a73
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-28 13:51:39 +0530

    feat:Tuples.rs file added

---
+++ Primitives/Tuples.rs
@@ -0,0 +1,6 @@
+/*
+Tuple:
+  1. A tuple is a collection of values of different types. 
+  2. Tuples are constructed using parentheses (), and each tuple itself is a value with type signature (T1, T2, ...), where T1, T2 are the types of its members.
+  3. Functions can use tuples to return multiple values, as tuples can hold any number of values.
+*/
