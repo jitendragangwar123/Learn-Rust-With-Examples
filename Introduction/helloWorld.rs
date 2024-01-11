eb35fab811e00f6b6da5fbea4f2751db943b7058
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-01-11 23:36:12 +0530

    feat:function added

--- Introduction/helloWorld.rs
+++ Introduction/helloWorld.rs
@@ -9,11 +9,31 @@ Note: `println!` is a macro that prints text to the console.
 */
 
 //helloWorld.rs
-// This is the main function.
 fn main() {
-    // Statements here are executed when the compiled binary is called.
-    // Print text to the console.
-    println!("Hello World!");
+    println!("Hello, world!");
+    
+    let x: i32 = 42;
+    let pi: f64 = 3.14159;
+    let is_rust_fun: bool = true;
+    let letter_a: char = 'a';
+    //function
+    fn add(x: i32, y: i32) -> i32 {
+        x + y
+    }
+    //loops
+    let x = 42;
+
+    if x >= 0 {
+        println!("x is non-negative");
+    } else {
+        println!("x is negative");
+    }
+
+    let mut i = 1;
+
+    while i <= 5 {
+        println!("{}", i);
+        i += 1;
+    }
 }
 
-// Output :- Hello World!
