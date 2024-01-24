3313d9e812ef4c28f0ed75fc28327ae4078159ef
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-01-24 23:02:12 +0530

    feat:user input as a number

--- Array/User_Input.rs
+++ Array/User_Input.rs
@@ -12,3 +12,16 @@ fn main() {
 
     println!("Your name is: {}", s.trim());
 }
+
+
+// taking user input as a number
+
+use std::io;
+
+fn main() {
+    let mut a = String::new();
+
+    io::stdin().read_line(&mut a).unwrap(); 
+    let a: i32 = a.trim().parse().unwrap();
+    println!("Your number is: {}", a); 
+}
