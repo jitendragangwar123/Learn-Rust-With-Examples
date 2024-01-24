0fa1798c2050c2b5bb854f4b0bd9f2f287587e4f
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-01-24 23:07:46 +0530

    feat:trim the string input

--- Array/User_Input.rs
+++ Array/User_Input.rs
@@ -1,5 +1,4 @@
 use std::io::{self, Write};
-
 fn main() {
     let mut s = String::new();
 
@@ -15,13 +14,24 @@ fn main() {
 
 
 // taking user input as a number
-
 use std::io;
-
 fn main() {
     let mut a = String::new();
 
     io::stdin().read_line(&mut a).unwrap(); 
     let a: i32 = a.trim().parse().unwrap();
     println!("Your number is: {}", a); 
+}
+
+// trim the string input
+use std::io;
+fn main() {
+    let mut x = String::new();
+
+    io::stdin()
+        .read_line(&mut x)
+        .expect("Failed to read line");
+
+    x = x.trim().to_string();  // to remove any trailing newline
+    println!("Hello {}", x);
 }
