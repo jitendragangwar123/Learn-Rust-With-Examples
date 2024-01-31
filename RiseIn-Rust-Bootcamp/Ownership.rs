cab1f45e44d90654336a1a63415322279ad58f4a
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-01-31 18:41:41 +0530

    feat:Ownership.rs added

---
+++ RiseIn-Rust-Bootcamp/Ownership.rs
@@ -0,0 +1,52 @@
+/*
+Strings:
+      Strings in Rust are dynamically allocated in memory, which means that they can grow or shrink as needed. 
+*/
+let my_string = String::from("hello, world!"); 
+
+/*
+- Rust also has a built-in string slice type, which is called &str.
+- String slices are references to a portion of a String or a string literal, and they are often used when you want to manipulate a string without taking ownership of it. 
+*/
+
+let my_string = String::from("hello, world!"); 
+let slice = &my_string[0..5]; 
+println!("{}", slice); 
+
+
+/*
+Ownership:
+    In Rust, when you create a value, you become its owner, and you are responsible for managing its memory.
+    It follows a "move semantics" model.
+    This means that when a value is assigned to a new variable or passed as an argument to a function, the ownership of the value is transferred to the new variable or function.
+    This ensures that there is always exactly one owner for each value, which helps to prevent common memory-related bugs such as double-freeing or use-after-free errors. 
+*/
+
+fn print_string(s: String) { 
+    println!("{}", s); 
+} 
+let my_string = String::from("hello, world!"); 
+print_string(my_string); 
+
+/*
+Stack And Heap:
+      In Rust, memory is divided into two main categories: the stack and the heap.
+      The stack is a region of memory that is used for static memory allocation, which means that the size of the memory is determined at compile-time.
+      The heap, on the other hand, is a region of memory that is used for dynamic memory allocation, which means that the size of the memory can change at runtime.
+*/
+
+let x = 5; // stored on the stack 
+let y = String::from("hello"); // stored on the heap 
+println!("x = {}, y = {}", x, y); 
+
+/*
+- If the variable needs to be stored on the heap, Rust allocates memory for it on the heap and creates a reference to the memory on the stack. 
+- In Rust, each variable has a specific owner, which is responsible for allocating and deallocating memory for the variable.
+- When the program exits the main function, Rust automatically deallocates the memory that was allocated for x, y, and z.
+- Ownership is an important concept in Rust because it helps to ensure memory safety and prevent common bugs such as null pointer errors, dangling pointers, and memory leaks.
+*/
+
+let x = 5; // stored on the stack 
+let y = String::from("hello"); // stored on the heap 
+let z = y; // ownership of y is moved to z 
+println!("x = {}, z = {}", x, z); 
