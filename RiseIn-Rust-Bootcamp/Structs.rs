a3c358ff48d8def7d16c884cebc8117eae28b5a5
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-02-04 23:39:06 +0530

    feat:Structs.rs added

---
+++ RiseIn-Rust-Bootcamp/Structs.rs
@@ -0,0 +1,9 @@
+/*
+Structs, short for structures, are a way to group related data together in a custom data type.
+*/
+
+struct Book {
+    title: String,
+    author: String,
+    publication_year: u32,
+}
