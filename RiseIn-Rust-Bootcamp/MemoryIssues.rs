b0f7fae137cd3260184407870f390b8b958c2425
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-02-05 12:14:35 +0530

    feat:memory safety using rust

--- RiseIn-Rust-Bootcamp/MemoryIssues.rs
+++ RiseIn-Rust-Bootcamp/MemoryIssues.rs
@@ -12,3 +12,18 @@ Here are some common memory-related problems that developers might face:
 3. Data Races: A data race occurs when two or more threads access the same memory location concurrently, and at least one of them performs a write operation. 
                Data races can lead to unpredictable behavior and hard-to-debug issues in your program.
 */
+
+/*
+Addressing Memory Safety Concerns with Rust's Innovative Features:
+1.Buffer Overflow: Rust enforces strict bounds checking on arrays and other collections, preventing programs from writing more data than the buffer can hold.
+2.Use-After-Free: Rust's ownership system and borrowing rules ensure that memory is managed safely. 
+                  Once a piece of memory is deallocated or moved, the compiler prevents further access to it.
+3.Data Races: By enforcing that either a single mutable reference or multiple immutable references can access the data at any given time, Rust ensures safe concurrent access to memory locations.            
+*/
+
+/*
+Variable Lifetimes in Rust for Memory Safety:
+1.When you create a variable, Rust allocates memory for it, and the variable becomes valid.
+2.As the program runs, the variable can be used within its scope, and Rust ensures that no other part of the code can modify or access the memory it occupies in an unsafe way.
+3.Once the variable goes out of scope, Rust automatically deallocates its memory, making it unavailable for further use.
+*/
