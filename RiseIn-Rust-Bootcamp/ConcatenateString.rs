adca944e2aba418acc8e260f666e41a05650f62a
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-02-06 23:02:22 +0530

    chore:comments added

--- RiseIn-Rust-Bootcamp/ConcatenateString.rs
+++ RiseIn-Rust-Bootcamp/ConcatenateString.rs
@@ -1,16 +1,12 @@
 use std::io::{self, Write};
-
 // Function to concatenate two string slices and return a new String
 fn concatenate_strings(str1: &str, str2: &str) -> String {
     // Create a new String
     let mut result = String::new();
-
     // Append the contents of the first string slice
     result.push_str(str1);
-
     // Append the contents of the second string slice
     result.push_str(str2);
-
     // Return the concatenated result
     result
 }
@@ -18,18 +14,20 @@ fn concatenate_strings(str1: &str, str2: &str) -> String {
 fn main() {
     // Prompt the user for input
     println!("Enter the first string:");
+    // To show the prompt message
     io::stdout().flush().unwrap();
+    // Initialize the new string
     let mut string1 = String::new();
     io::stdin().read_line(&mut string1).expect("Failed to read line");
-
+    // Prompt the user for input
     println!("Enter the second string:");
+    // To show the prompt message
     io::stdout().flush().unwrap();
+    // To initialize the new string
     let mut string2 = String::new();
     io::stdin().read_line(&mut string2).expect("Failed to read line");
-
     // Call the concatenate_strings function with references to user-input strings
     let concatenated_string = concatenate_strings(string1.trim(), string2.trim());
-
     // Print the concatenated result
     println!("Concatenated String: {}", concatenated_string);
 }
