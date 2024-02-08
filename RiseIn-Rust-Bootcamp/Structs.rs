36e034c7c8ddd5362e47a196fd2bb8ed53ef8ad1
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-02-08 18:30:36 +0530

    feat:Structs.rs updated

--- RiseIn-Rust-Bootcamp/Structs.rs
+++ RiseIn-Rust-Bootcamp/Structs.rs
@@ -16,3 +16,49 @@ struct Point {
     x: f32,
     y: f32,
 }
+
+/*
+Creating Instances of Structs:
+*/
+let point = Point { x: 1, y: 2 };
+
+/*
+Accessing Struct Fields: 
+*/
+struct Person {
+    name: String,
+    age: u32,
+}
+ 
+fn main() {
+    let alice = Person {
+        name: String::from("Alice"),
+        age: 30,
+    };
+ 
+    // Accessing the 'name' and 'age' fields
+    println!("Name: {}", alice.name);
+    println!("Age: {}", alice.age);
+}
+
+/*
+Example: You can also modify the values of the fields if the struct instance is mutable.
+*/
+struct Person {
+    name: String,
+    age: u32,
+}
+fn main() {
+    let mut alice = Person {
+        name: String::from("Alice"),
+        age: 30,
+    };
+ 
+    // Modifying the 'age' field
+    alice.age = 31;
+    println!("Updated Age: {}", alice.age);
+}
+
+
+/*
+*/
