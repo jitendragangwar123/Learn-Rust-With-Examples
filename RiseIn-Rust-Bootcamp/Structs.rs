29fb426bb68b0be50ec93227cd9341c7e89b8a56
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-02-09 23:10:51 +0530

    feat:Structs.rs updated

--- RiseIn-Rust-Bootcamp/Structs.rs
+++ RiseIn-Rust-Bootcamp/Structs.rs
@@ -61,4 +61,28 @@ fn main() {
 
 
 /*
+Implementing Methods for Structs:
 */
+
+#[derive(Debug)]
+struct Rectangle {
+    width: f64,
+    height: f64,
+}
+ 
+impl Rectangle {
+    fn area(&self) -> f64 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let my_rectangle = Rectangle {
+        width: 10.0,
+        height: 5.0,
+    };
+ 
+    let area = my_rectangle.area();
+    println!("The area of the rectangle is: {}", area);
+}
+
