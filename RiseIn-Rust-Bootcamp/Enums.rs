9b00367a76661e693336b70286e45c02c521ce68
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-02-14 17:16:08 +0530

    feat:enums with methods

--- RiseIn-Rust-Bootcamp/Enums.rs
+++ RiseIn-Rust-Bootcamp/Enums.rs
@@ -1,5 +1,5 @@
 /*
-Enums:
+Enums: Rust that allows you to represent multiple related values within a single data type(enum).
 */
 enum Weather {
     Sunny,
@@ -74,5 +74,62 @@ fn main() {
 
     for msg in messages {
         process_message(&msg);
+    }
+}
+
+/*
+‘if let’ Syntax:
+*/
+
+enum Animal {
+    Dog(String),
+    Cat(String),
+    Bird(String),
+}
+
+fn main() {
+    let my_pet1 = Animal::Cat("Fluffy".to_string());
+    let my_pet2 = Animal::Dog("Peter".to_string());
+
+    if let Animal::Cat(name) = my_pet1 {
+        println!("My cat's name is: {}", name);
+    }if let Animal::Dog(name) = my_pet2 {
+        println!("My Dog's name is: {}",name);
+    } else {
+        println!("My pet is not a cat.");
+    }
+}
+
+/*
+Enums and Methods in Rust:
+*/
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(u8, u8, u8),
+}
+
+impl Message {
+    fn call(&self) {
+        match self {
+            Message::Quit => println!("Quit"),
+            Message::Move { x, y } => println!("Move to x: {}, y: {}", x, y),
+            Message::Write(s) => println!("Write: {}", s),
+            Message::ChangeColor(r, g, b) => println!("Change color to R: {}, G: {}, B: {}", r, g, b),
+        }
+    }
+}
+
+fn main() {
+    let message = vec![
+        Message::Write(String::from("Hello, Rust!")),
+        Message::Quit,
+        Message::Move{x:1,y:2},
+        Message::ChangeColor(1,2,3),
+    ];
+    
+    for msg in message{
+        msg.call();
     }
 }
