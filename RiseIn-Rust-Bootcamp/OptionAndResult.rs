0246a5fc2d6d1b5951d3c588bfe513d66f72c1cc
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-02-15 11:51:27 +0530

    feat:Result in rust

--- RiseIn-Rust-Bootcamp/OptionAndResult.rs
+++ RiseIn-Rust-Bootcamp/OptionAndResult.rs
@@ -32,3 +32,40 @@ fn main() {
         None => println!("The square root of {} is not a real number.", number),
     }
 }
+
+/*
+Introduction to Result:
+        1.Result is another essential enum in Rust, used for handling errors and returning meaningful information about the cause of the error.
+        2.It is a generic enum that can hold two types T and E. 
+        3.Result has two variants:
+                Ok(T): Represents a successful operation with a value of type T
+                Err(E): Represents an error with a value of type E
+
+*/
+
+pub enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+
+/*
+Example of using Result:
+*/
+fn divide(a: f64, b: f64) -> Result<f64, String> {
+    if b == 0.0 {
+        Err("Division by zero is not allowed.".to_string())
+    } else {
+        Ok(a / b)
+    }
+}
+
+fn main() {
+    let a = 10.0;
+    let b = 0.0;
+    let division_result = divide(a, b);
+
+    match division_result {
+        Ok(value) => println!("{} divided by {} is: {}", a, b, value),
+        Err(error_message) => println!("Error: {}", error_message),
+    }
+}
