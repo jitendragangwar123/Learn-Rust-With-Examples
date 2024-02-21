e87bf12ac5a4ede45a7c887215912dc5214b305f
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-02-22 00:16:26 +0530

    feat:implementing traits for existing types

--- RiseIn-Rust-Bootcamp/Traits.rs
+++ RiseIn-Rust-Bootcamp/Traits.rs
@@ -9,7 +9,6 @@ Basics of Generics:
   - Generics are a powerful feature that allows you to write flexible and reusable code by defining functions, 
     structs, enums, and even traits that work with multiple types, without having to duplicate the code for each type.
 */
-
 fn identity<T>(value: T) -> T {
     value
 }
@@ -33,21 +32,17 @@ Implementing Traits:
       and the name of the type you want to implement the trait for.
     - You provide the method implementations in a block.
 */
-
 trait Speak {
     fn speak(&self);
 }
-
 struct Dog {
     name: String,
 }
-
 impl Speak for Dog {
     fn speak(&self) {
         println!("{} says: Woof!", self.name);
     }
 }
-
 fn main(){
     let d = Dog{name:String::from("Tom")}; 
     d.speak();
@@ -59,28 +54,23 @@ Implementing Traits for Custom Types (Structs, Enums):
 trait Display {
     fn display(&self) -> String;
 }
-
 struct Circle {
     radius: f64,
 }
-
 struct Rectangle {
     width: f64,
     height: f64,
 }
-
 impl Display for Circle {
     fn display(&self) -> String {
         format!("Circle with radius: {}", self.radius)
     }
 }
-
 impl Display for Rectangle {
     fn display(&self) -> String {
         format!("Rectangle with width: {} and height: {}", self.width, self.height)
     }
 }
-
 fn main(){
     let c = Circle{radius:20.0}; 
     let r = Rectangle{width:4.0,height:5.0};
@@ -89,4 +79,20 @@ fn main(){
 
 /*
 Implementing Traits for Existing Types:
+    - You can only implement a trait for a type if either the trait or the type is defined within your own crate.
 */
+trait Display {
+    fn display(&self) -> String;
+}
+impl Display for String {
+    fn display(&self) -> String {
+        self.clone()
+    }
+}
+fn display_something<T: Display>(item: &T) {
+    println!("{}", item.display());
+}
+fn main() {
+    let my_string = String::from("Hello, world!");
+    display_something(&my_string);
+}
