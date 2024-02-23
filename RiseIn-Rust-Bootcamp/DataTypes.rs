4ae930931621ff6a125dd4887f30d88b77efc7b9
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-02-23 12:13:09 +0530

    fix:categorise the data-types

--- RiseIn-Rust-Bootcamp/DataTypes.rs
+++ RiseIn-Rust-Bootcamp/DataTypes.rs
@@ -1,13 +1,20 @@
 /*
-Booleans: bool, which can have the values true or false
-Integers: i8, i16, i32, i64, i128, u8, u16, u32, u64, u128, which are signed and unsigned integers of different sizes
-Floating-point numbers: f32 and f64, which are single-precision and double-precision floating-point numbers, respectively
-Characters: char, which represents a single Unicode character
-Strings: &str and String, which represent a sequence of Unicode characters
-Arrays: [T; N], which represent a fixed-size array of elements of type T
-Slices: [T] and &[T], which represent a variable-size sequence of elements of type T
-Tuples: (T1, T2, ..., Tn), which represent a fixed-size sequence of elements of different types
-Unit type: (), which represents an empty tuple and is used when no value is needed
+1.Scalar Type:
+    Booleans: bool, which can have the values true or false
+    Integers: i8, i16, i32, i64, i128, u8, u16, u32, u64, u128, which are signed and unsigned integers of different sizes
+    Floating-point numbers: f32 and f64, which are single-precision and double-precision floating-point numbers, respectively
+    Characters: char, which represents a single Unicode character
+
+2.Compound Type:
+    Strings: &str and String, which represent a sequence of Unicode characters
+    Arrays: [T; N], which represent a fixed-size array of elements of type T
+    Slices: [T] and &[T], which represent a variable-size sequence of elements of type T
+    Tuples: (T1, T2, ..., Tn), which represent a fixed-size sequence of elements of different types
+    Unit type: (), which represents an empty tuple and is used when no value is needed
+
+3. Custom Types:
+    Stucts:
+    Enums:
 */
 
 //Booleans:
