aa663fa210ddc80f05e3c3fd68c32a4c5d8ae64c
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-02-23 17:37:08 +0530

    feat:return multiple values using tuples

--- RiseIn-Rust-Bootcamp/Functions.rs
+++ RiseIn-Rust-Bootcamp/Functions.rs
@@ -6,12 +6,42 @@ fn function_name(parameter1: type1, parameter2: type2) -> return_type {
     return value;
 }
 
+// define the type of parameters
+fn another_fn(num:i32,letter:char){
+    println!("The value of num is: {} and letter is : {}",num,letter);
+}
+fn main(){
+    println!("Hello");
+    another_fn(42,'A');
+}
+
+// statements and expressions
+fn main(){
+    let y={
+        let x=3;
+        x+1
+    };
+    println!("The value of y is :{}",y); // 4
+}
+
+
 // Function Arguments and Return Types
 fn get_greeting() -> String {
     return String::from("Hello, Rust!");
 }
-println!("{}",get_greeting());
+fn main(){
+    println!("{}",get_greeting());
+}
 
+// Return multiple values using tuples
+fn sum(num1:i32,num2:i32) -> (i32,i32){
+    (num1+num2,num1-num2)
+}
+fn main(){
+    let x=sum(6,5);
+    //println!("Sum is:{} and Diff is: {}",x.0,x.1);
+    println!("The sum and diff is: {:?}",x);
+}
 
 // Passing Parameters to a Function
 fn add_numbers(x: i32, y: i32) -> i32 {
