8ef2c89cd308331f1d9d585813f977a0c8a60fb0
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-03-14 23:29:20 +0530

    feat:Slice syntax added

--- RiseIn-Rust-Bootcamp/SliceType.rs
+++ RiseIn-Rust-Bootcamp/SliceType.rs
@@ -1,3 +1,10 @@
 /*
-Slice: Slice are a refernce to a contiguous sequence of elements in collection.
+Slice: Slice are a reference to a contiguous sequence of elements in the collection.
+       They are a view into the original collection and do not store any data themselves.
+       Slices are used to give a part of a collection to a function or to iterate over a part of the collection. 
 */
+
+/*
+Syntax : &[T]
+T --> Type of the elements in the collection
+& --> Reference to the collection
