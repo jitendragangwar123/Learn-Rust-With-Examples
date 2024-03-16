828f2ed7a3bf9e3d25b6e2b9efbe55dc62aeb0db
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-03-16 21:37:19 +0530

    feat:Slice type example added

--- RiseIn-Rust-Bootcamp/SliceType.rs
+++ RiseIn-Rust-Bootcamp/SliceType.rs
@@ -8,3 +8,23 @@ Slice: Slice are a reference to a contiguous sequence of elements in the collect
 Syntax : &[T]
 T --> Type of the elements in the collection
 & --> Reference to the collection
+*/
+
+//Example
+pub fn main() {
+    //slice of an array of characters
+    let arr: [char; 5] = ['a', 'b', 'c', 'd', 'e'];
+    let slice: &[char] = &arr[1..3];
+    println!("{:?}", slice);
+
+    //slice of a vector of integers
+    let vec: Vec<i32> = vec![10, 20, 30, 40, 50];
+    let slice = &vec[3..4];
+    println!("{:?}", slice);
+
+    //slice for strings
+    let s:String=String::from("hello world");
+    let hello = &s[1..2];
+    let world= &s[8..11];
+    println!("{:?} {:?}",hello,world);
+}
