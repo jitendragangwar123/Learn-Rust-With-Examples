e4724e4edc8926fb2f9757f917d11a960fba28f9
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-03-17 16:15:03 +0530

    feat:tuple structs added

--- Structs/Introduction.rs
+++ Structs/Introduction.rs
@@ -76,3 +76,13 @@ pub fn main(){
     println!("{:?}",user1);
 }
 
+//tuple structs
+struct Color(i32,i32,i32);
+struct Point(i32,i32,i32);
+
+fn main(){
+    let black=Color(0,0,0);
+    let origin=Point(0,0,0);
+    println!("Black: {}, {}, {}",black.0,black.1,black.2);
+    println!("Origin: {}, {}, {}",origin.0,origin.1,origin.2);
+}
