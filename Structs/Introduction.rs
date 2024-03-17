81cf0017225ae84270d09a05df95e0115672bbd4
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-03-17 14:36:44 +0530

    feat:create multiple instances of structs

--- Structs/Introduction.rs
+++ Structs/Introduction.rs
@@ -3,6 +3,7 @@
 // used to create more complex data types
 // stucts are immutable by default
 
+//Example:1
 #[derive(Debug)]
 struct User{
     name:String,
@@ -23,3 +24,55 @@ pub fn main(){
     user1.name=String::from("Jay");
     println!("Name: {}",user1.name);
 }
+
+//Example:2
+#[derive(Debug)]
+struct User{
+    name:String,
+    email:String,
+    is_active:bool,
+    age:u8
+}
+
+pub fn main(){
+    let user1=build_user(String::from("Jay"), String::from("jay@gmail.com"));
+    println!("{:?}",user1);
+}
+
+fn build_user(name:String,email:String)->User{
+    User {
+        name,
+        email,
+        is_active:true,
+        age:34
+    }
+}
+
+//Example:3
+#[derive(Debug)]
+struct User{
+    name:String,
+    email:String,
+    is_active:bool,
+    age:u8
+}
+
+pub fn main(){
+    let user1 = User{
+        name:String::from("Jhone"),
+        email:String::from("jhone@gmail.com"),
+        is_active:true,
+        age:68
+    };
+    
+    //create the new instance
+    let user2 = User{
+        name:String::from("Jacob"),
+        email:user1.email.clone(),//one more copy in heap
+        is_active:true,
+        age:43
+    };
+    println!("{:?}",user2);
+    println!("{:?}",user1);
+}
+
