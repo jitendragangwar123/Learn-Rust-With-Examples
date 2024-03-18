4af3b85149023e43ed50fa20161ee93e70f18501
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-03-18 23:19:43 +0530

    feat:area of the rectangle

---
+++ Structs/StructsExamples.rs
@@ -0,0 +1,26 @@
+//Example: to find the area of the rectangle 
+//with tuples
+fn main(){
+    let rect1=(30,40);
+    println!("Area of the Rectangle: {}",area_of_rectangle(rect1));
+}
+
+fn area_of_rectangle(dimensions:(u32,u32))->u32{
+    dimensions.0*dimensions.1
+}
+
+//with structs
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 40,
+    };
+    println!("Rectangle Params: {:#?}",rect1);
+    println!("Area of the Rectangle: {}", area_of_rectangle(&rect1));
+}
