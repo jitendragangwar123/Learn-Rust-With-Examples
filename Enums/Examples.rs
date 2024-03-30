953017b39487a201f506ac744d9750792416e4f5
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-03-30 23:28:50 +0530

    feat:enums value type

--- Enums/Examples.rs
+++ Enums/Examples.rs
@@ -1,12 +1,28 @@
 //We can define the types of the enum's values 
+//Example:1
 #[derive(Debug)]
 enum IpAddressTypes{
-    v4(String),
+    v4(String), //String type
     v6(String),
 }
 
 pub fn main(){
    let home=IpAddressTypes::v4(String::from("127.0.0.1"));
+   println!("{:?}",home);
+
+   let loopback=IpAddressTypes::v6(String::from("::1"));
+   println!("{:?}",loopback);
+}
+
+
+//Example:2
+#[derive(Debug)]
+enum IpAddressTypes{
+    v4(u8,u8,u8,u8), //u8 type
+    v6(String),
+}
+fn main(){
+   let home=IpAddressTypes::v4(127,0,0,1);
    println!("{:?}",home);
 
    let loopback=IpAddressTypes::v6(String::from("::1"));
