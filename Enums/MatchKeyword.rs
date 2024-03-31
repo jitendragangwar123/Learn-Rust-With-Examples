05d10dd85be6f1af2166ff4d65c5bab72f766f8d
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-03-31 15:45:36 +0530

    feat:multiple enums in rust

--- Enums/MatchKeyword.rs
+++ Enums/MatchKeyword.rs
@@ -1,4 +1,5 @@
-// match keyword use to compares values against a series of patterns.
+// match keyword is used to compare values against a series of patterns.
+//Example:1
 #[allow(dead_code)]
 enum Direction{
     Up,
@@ -19,4 +20,38 @@ fn move_player(direction:Direction){
 pub fn main(){
     let player_direction=Direction::Up;
     move_player(player_direction);
+}
+
+//Example:2
+#[allow(dead_code)]
+enum Coin{
+    Penny,
+    Nickel,
+    Dine,
+    Quarter(Rarity),
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Rarity{
+    Common,
+    Uncommon,
+    Rare,
+    Epic,
+    Legendary,
+}
+
+fn value_in_cents(coin:Coin)->u8{
+    match coin{
+        Coin::Penny=>1,
+       Coin::Nickel=>5,
+       Coin::Dine=>10,
+       Coin::Quarter(rarity)=>25,
+    }
+}
+
+pub fn main(){
+    let coin_value=Coin::Quarter(Rarity::Common);
+    println!("{}",value_in_cents(coin_value));
+    
 }
