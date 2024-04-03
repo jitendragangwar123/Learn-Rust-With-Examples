8bc2cfe18856ef20d6d80a7dfc09b09ee35a91b3
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-04-03 23:15:21 +0530

    feat:literals in rust

--- Primitives/Literals.rs
+++ Primitives/Literals.rs
@@ -1,7 +1,11 @@
-fn main() {
-    // Integer -> 4 bytes, float -> 8 bytes
-    let x: i32 = 3; // Explicitly specify x as an i32 integer
-    let y: f64 = 6.0; // Explicitly specify y as an f64 floating-point number
-    println!("Size of `x` is {} bytes", std::mem::size_of_val(&x));
-    println!("Size of `y` is {} bytes", std::mem::size_of_val(&y));
+fn main() { 
+   //integer -> 4 bytes, char -> 4 bytes, bool -> 1 byte, float -> 8 bytes
+    let x = 3; 
+    let y = 6.0;
+    let z:bool=true;
+    let c:char='c';
+    println!("size of `x` is {} bytes", std::mem::size_of_val(&x)); 
+    println!("size of `y` is {} bytes", std::mem::size_of_val(&y));    
+    println!("size of `z` is {} bytes", std::mem::size_of_val(&z)); 
+    println!("size of `c` is {} bytes", std::mem::size_of_val(&c)); 
 }
