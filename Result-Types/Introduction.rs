70cf0eaa7af519bd0d36b453a80694cfc8b3c5fb
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-04-08 17:59:23 +0530

    feat:result types in rust

---
+++ Result-Types/Introduction.rs
@@ -0,0 +1,11 @@
+use std::num::ParseIntError; 
+
+fn main() -> Result<(), ParseIntError> { 
+	let number_to_str = "GeeksforGeeks"; 
+	let num = match number_to_str.parse::<i32>() { 
+		Ok(num) => num, 
+		Err(e) => return Err(e), 
+	}; 
+	println!("{}", num); 
+	Ok(()) 
+} 
