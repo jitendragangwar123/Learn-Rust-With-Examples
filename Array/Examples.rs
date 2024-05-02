ce3795aa2f6769877e70ebfcbeefad8b81634524
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-05-02 23:38:22 +0530

    feat:pass by value and pass by reference in rust

---
+++ Array/Examples.rs
@@ -0,0 +1,37 @@
+// pass by value
+fn main() {
+  let array = [1,2,3,4];
+  change_array(array); 
+  print!("Original array {:?}",array);
+}
+
+fn change_array(mut array:[i32;4]){
+   for i in 0..4 {
+     array[i] = 0;
+   }
+  println!("Changed array {:?}",array);
+}
+/*
+Output:
+Changed array [0, 0, 0, 0]
+Original array [1, 2, 3, 4]
+*/
+
+//Pass by reference
+#![allow(unused)]
+fn main() {
+  let mut array = [1,2,3,4,5];
+  change_array(&mut array);
+  print!("Original array {:?}",array);
+}
+fn change_array(array:&mut [i32;5]){
+  for i in 0..5 {
+  	array[i] = 0;
+  }
+ println!("Changed array {:?}",array);
+}
+
+/*
+Changed array [0, 0, 0, 0, 0]
+Original array [0, 0, 0, 0, 0]
+*/
