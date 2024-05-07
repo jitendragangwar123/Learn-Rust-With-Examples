cc27579947053852857ca863f2fc50a8e57c6bb2
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2024-05-07 23:47:09 +0530

    feat: factorial of a number

---
+++ Examples/Factorial.rs
@@ -0,0 +1,14 @@
+// to find the factorial of the number
+fn fact(n: i32) -> i32 {
+    if n == 0 {
+        1 
+    } else if n > 0 {
+        n * fact(n - 1) 
+    } else {
+        -1 
+    }
+}
+
+fn main() {
+    println!("{:?}", fact(-6));
+}
